421277c5914d9767b7d6109f60ec25b08d5c8b59
Basavaraj Badiger <basukb@gmail.com>
2020-07-07 23:54:40 +0800

    Update the service

---
+++ ExxchangeRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExxchangeRateMonitorService
+{
+    public class ExxchangeRate
+    {
+        public string Base { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Currency Rates { get; set; }
+    }
+
+    public class Currency
+    {
+        public string INR { get; set; }
+
+        public string SGD { get; set; }
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace ExxchangeRateMonitorService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HttpClient _httpClient;
 
         public Worker(ILogger<Worker> logger)
         {
@@ -20,11 +23,13 @@ namespace ExxchangeRateMonitorService
         public override Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Service started at:{time}", DateTimeOffset.Now);
+            _httpClient = new HttpClient();
             return base.StartAsync(cancellationToken);
         }
         
         public override Task StopAsync(CancellationToken cancellationToken)
         {
+            _httpClient.Dispose();
             _logger.LogInformation("Service has been stopped at: {time}", DateTimeOffset.Now);
             return base.StopAsync(cancellationToken);
         }
@@ -33,7 +38,18 @@ namespace ExxchangeRateMonitorService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Task executed at: {time}", DateTimeOffset.Now);
+                var response = await _httpClient.GetAsync("https://api.exchangeratesapi.io/latest?base=SGD&symbols=SGD,INR");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var exxchange = JsonConvert.DeserializeObject<ExxchangeRate>(content).Rates;
+                    _logger.LogInformation($"ExxchangeRate 1 SGD = {exxchange.INR} INR  at: {DateTimeOffset.Now}");
+                }
+                else
+                {
+                    _logger.LogInformation($"An error occured with status code {response.StatusCode}");
+                }
+               
                 await Task.Delay(60000, stoppingToken);
             }
         }
